24a9fa9ad3449f54595d48eec5e0852835b44d12
sparklabel <psk492@gmail.com>
2022-08-25 16:22:11 +0900

    add test code

---
+++ main_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+from main import hello
+
+class MainTest(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello(), "Hello World!")
+
+
+if __name__ == "__main__":
+    unittest.main()
